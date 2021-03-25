70eba01c3a455da4eef290a8db40f211c6f5197c
Tarik <tarikalic649@gmail.com>
2021-03-25 01:19:42 +0100

    Addedd ex07

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -5,10 +5,11 @@ const range = {
   num2: 88.8,
   average: 45.325,
 };
-// Only change code below this line
 
-const middle = ({ max, min }) => (max + min) / 2.0;
+function middle(range1) {
+  const { max: max, min: min } = range1;
 
-// Only change code above this line
+  return (max + min) / 2;
+}
 console.log(middle(range));
 module.exports = middle;
